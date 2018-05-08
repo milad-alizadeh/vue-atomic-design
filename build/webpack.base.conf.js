dd6af1aa465796e0e0070445baca7a1f656c3a3f
Milad Alizadeh <milaadd@gmail.com>
2018-05-08 15:31:29 +0100

    update base config

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -36,8 +36,8 @@ module.exports = {
     alias: {
       'vue$': 'vue/dist/vue.esm.js',
       '@': resolve('src'),
-      components: path.resolve(__dirname, './src/components/'),
-      containers: path.resolve(__dirname, './src/containers/'),
+      components: path.resolve(__dirname, '../src/components/'),
+      containers: path.resolve(__dirname, '../src/containers/'),
       store: path.resolve(__dirname, './src/store/')
     }
   },
