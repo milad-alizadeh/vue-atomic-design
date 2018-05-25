ed88af70e2db0f0b294a64b2c50de0fe7ce11e77
Milad Alizadeh <milaadd@gmail.com>
2018-05-25 16:40:34 +0100

    update addons

--- private/storybook/config.js
+++ private/storybook/config.js
@@ -1,9 +1,17 @@
 import { configure } from '@storybook/vue';
+import { setOptions } from '@storybook/addon-options';
 import Vue from 'vue';
 import Vuex from 'vuex';
 import '../../src/scss/base.scss';
 
 Vue.use(Vuex);
+
+setOptions({
+  hierarchyRootSeparator: / - /,
+  name: 'Vue Boilerplate',
+  url: '#'
+});
+
 
 // Require all the .stories.js files from all components
 const req = require.context('components', true, /.stories.js$/);
